0073448d0d340f4c63b3d9ce5717e5765917d286
SvetaPogosova <svet.pogosova@gmail.com>
2023-09-06 22:04:40 +0300

    HW4

--- src/HW/4/Input.tsx
+++ src/HW/4/Input.tsx
@@ -1,14 +1,27 @@
+import {ChangeEvent} from "react";
+
 type InputPropsType = {
-	currentText: any // НУЖНО ПРОТИПИЗИРОВАТЬ
-	setCurrentText: any // НУЖНО ПРОТИПИЗИРОВАТЬ
+    currentText: string
+    setCurrentText: (newTitle: string) => void
 };
 
 export const Input = (props: InputPropsType) => {
-	const onChangeHandler = (event: 'НУЖНО ПРОТИПИЗИРОВАТЬ') => {
-		// НУЖНО ДОПИСАТЬ
-	};
+    const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+        props.setCurrentText(event.currentTarget.value)
 
-	// return (
-	//   <input id={'hw04-input'} type="text" value={props.currentText} onChange={onChangeHandler} />
-	// );
+
+
+
+
+        console.log(props.setCurrentText)
+
+
+
+
+
+    };
+
+    return (
+        <input id={'hw04-input'} type="text" value={props.currentText} onChange={onChangeHandler}/>
+    );
 };
